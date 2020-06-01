2f976bdbf1ee05fec325af82b76f58ab644d08cc
Archaeologist03 <omniiknight@hotmail.com>
2020-06-01 23:59:35 +0200

    Animation example - #3

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 const Home = () => {
   return (
-    <div className="home container">
-      <h2>
-        Welcome to Pizza Joint
-      </h2>
-      <Link to="/base">
-        <button>
+    <motion.div
+      className='home container'
+      animate={{ marginTop: 200, opacity: 0.2, rotateZ: 180 }}
+    >
+      <h2>Welcome to Pizza Joint</h2>
+      <Link to='/base'>
+        <motion.button animate={{ scale: 1.5 }}>
           Create Your Pizza
-        </button>
+        </motion.button>
       </Link>
-    </div>
-  )
-}
+    </motion.div>
+  );
+};
 
 export default Home;
